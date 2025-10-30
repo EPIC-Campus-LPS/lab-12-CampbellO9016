0d64a8117a1dc8883f5629861063cc22fd0b0786
CampbellO9016 <campbellosher@icloud.com>
2025-10-30 11:24:47 -0600

    Not done finish insertion and merge

---
+++ src/lab_12/QuadSorts.java
@@ -0,0 +1,56 @@
+package lab_12;
+
+public class QuadSorts {
+	public static int[] bubbleSort(int[] arr) {
+		for(int i = 0; i < arr.length; i ++) {
+			for(int j = 0; j < arr.length - i - 1; j ++) {
+				if(arr[j+1] < arr[j]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		return arr;
+	}
+	public static int[] selectionSort(int[] arr) {
+		for(int i = 0; i < arr.length; i ++) {
+			int stemp = Integer.MAX_VALUE;
+			int pos = 0;
+			for(int j = i; j < arr.length; j ++) {
+				if(arr[j] < stemp) {
+					stemp = arr[j];
+					pos = j;
+				}
+			}
+			arr[pos] = arr[i];
+			arr[i] = stemp;			
+		}
+		return arr;
+	}
+	public static int[] insertionSort(int[] arr) {
+		int[] newar = new int[arr.length];
+		for(int i = 0; i<arr.length; i ++) {
+			newar[i] = 0;
+		}
+		newar[0] = arr[0];
+		for(int i = 1; i < arr.length; i ++) {
+			for(int j = i; j < arr.length; j ++) {
+				if(arr[j] <= newar[0]) {
+//					int temp = newar[0];
+//					newar[0] = arr[j];
+					for(int k = 0; k < arr.length; k ++) {
+						//I think put the loops for each inside of ifs for up or down
+					}
+				}
+				else if(arr[j] > newar[0]) {
+					
+				}
+			}
+		}
+		return arr;
+	}
+//	public static int[] mergeSort(int[] arr) {
+//		
+//	}
+}
---
+++ src/lab_12/SortingTester.java
@@ -0,0 +1,49 @@
+package lab_12;
+
+public class SortingTester {
+
+	public static void main(String[] args) {
+		System.out.println("Bubble Sort");
+		int[] nums = {4, 5, 7, 2, 1};
+		for(int n: nums) {
+			System.out.println(n);
+		}
+		System.out.println();
+		QuadSorts.bubbleSort(nums);
+		for(int n: nums) {
+			System.out.println(n);
+		}int[] nums2 = {4, 5, 7, 2, 1};
+		System.out.println("Selection Sort");
+		for(int n: nums2) {
+			System.out.println(n);
+		}
+		System.out.println();
+		QuadSorts.selectionSort(nums2);
+		for(int n: nums2) {
+			System.out.println(n);
+		}
+		int[] nums3 = {4, 5, 7, 2, 1};
+		System.out.println("Insertion Sort");
+
+		for(int n: nums3) {
+			System.out.println(n);
+		}
+		System.out.println();
+		QuadSorts.insertionSort(nums3);
+		for(int n: nums3) {
+			System.out.println(n);
+		}
+//		int[] nums4 = {4, 5, 7, 2, 1};
+//		System.out.println("Merge Sort");
+//		for(int n: nums4) {
+//			System.out.println(n);
+//		}
+//		System.out.println();
+//		QuadSorts.insertionSort(nums4);
+//		for(int n: nums4) {
+//			System.out.println(n);
+//		}
+		
+	}
+
+}
